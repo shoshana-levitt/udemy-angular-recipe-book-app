73fffb8865e06b01f9f5fd4c16aef1b27a7cd150
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-12-19 15:34:27 -0500

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'udemy-angular-recipe-shopping-list-app'`, () => {
+  it(`should have as title 'udemy-angular-recipe-book-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('udemy-angular-recipe-shopping-list-app');
+    expect(app.title).toEqual('udemy-angular-recipe-book-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('udemy-angular-recipe-shopping-list-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'udemy-angular-recipe-book-app app is running!'
+    );
   });
 });
